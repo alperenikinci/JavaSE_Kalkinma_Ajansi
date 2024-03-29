39c53660834671d44c98a43cdccfa0ab0ffdc3dd
alperenikinci <alperen.ikinci@hotmail.com>
2024-03-29 14:25:47 +0300

    Sayiyi disaridan almak icin scanner eklendi.

--- src/week03/methods/asalsayisorusu/Test.java
+++ src/week03/methods/asalsayisorusu/Test.java
@@ -1,14 +1,21 @@
 package week03.methods.asalsayisorusu;
 
+import java.util.Scanner;
+
 public class Test {
     //1008001
+    //5000001557l
     public static void main(String[] args) { /*main metodu kodları çalıştırabilmemiz için kullan bir metottur.*/
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Lutfen bir sayi giriniz : ");
+        long number = scanner.nextLong();
+
         /*PrimeNumberCalculator sınıfının parametresiz bir nesnesini/instance'ını üretiyorum*/
         PrimeNumberCalculator primeNumberCalculatorNoArg = new PrimeNumberCalculator();//Argümansız ürettim.
-        primeNumberCalculatorNoArg.number=1; //argümansız ürettiğim için number değerini elle atadım.
+        primeNumberCalculatorNoArg.number=number; //argümansız ürettiğim için number değerini elle atadım.
 
         /*PrimeNumberCalculator sınıfının parametreli bir nesnesini/instance'ını üretiyorum*/
-        PrimeNumberCalculator primeNumberCalculatorWithArg = new PrimeNumberCalculator(5000001557l);//Sayıyı parametre kısmına, argüman olarak verdim.
+        PrimeNumberCalculator primeNumberCalculatorWithArg = new PrimeNumberCalculator(number);//Sayıyı parametre kısmına, argüman olarak verdim.
 
         long startTime = System.nanoTime();
         if(primeNumberCalculatorWithArg.asalSayiMiOptimize()){
@@ -21,6 +28,6 @@ public class Test {
         System.out.println("Programın nano saniye bazında calisma süresi : " + timeElapsed);
         System.out.println("Programın milisaniye bazında calisma süresi : " +(timeElapsed/1000000));
 
-
+        scanner.close();
     }
 }
